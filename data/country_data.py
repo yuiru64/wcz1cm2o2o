30564d27e08cea5fd636ac9c3418a4782cad511f
Christy_ <2433988494@qq.com>
2020-03-09 11:39:51 +0800

    Add some data

---
+++ data/country_data.py
@@ -0,0 +1,37 @@
+import xlrd
+import os
+
+os.chdir(os.path.dirname(__file__))
+
+def fromCountries(_list: list) -> dict:
+    table = xlrd.open_workbook("Countries of the world.xls")
+    sheet: xlrd.sheet.Sheet = table.sheet_by_index(0)
+    ret = {}
+    count = {}
+    for i in range(5, sheet.nrows):
+        key = sheet.cell_value(i, 0).strip()
+        if key is None:
+            break
+        if key in _list:
+            if key not in ret:
+                ret[key] = {
+                    "population": sheet.cell_value(i, 2),   # population
+                    "area": sheet.cell_value(i, 3)    # area
+                }
+                count[key] = 1
+            else:
+                ret[key] = {
+                    "population": ret[key]["population"] + sheet.cell_value(i, 2),   # population
+                    "area": ret[key]["area"] + sheet.cell_value(i, 3)    # area
+                }
+                count[key] += 1
+    for key in ret:
+        ret[key]["population"] /= count[key]
+        ret[key]["area"] /= count[key]
+    return ret
+
+if __name__ == "__main__":
+    ret = fromCountries([
+        "Tuvalu", "Nauru", "Palau"
+    ])
+    print(ret)
---
+++ data/sealevel_data.py
@@ -0,0 +1,18 @@
+import xlrd
+import os
+
+os.chdir(os.path.dirname(__file__))
+
+data = """0.04 0.11 0.24 0.41 0.63 0.90 1.2 1.6 2.0 2.5 3.6 5.5 9.7"""
+level = data.split(" ")
+level = [float(i) for i in level]
+
+def getLevel(year):
+    if year < 2010:
+        raise Exception("year should >= 2010")
+    return level[(year - 2010) // 10]\
+    + level[(year - 2010) // 10]\
+    + (year - 2010) % 10 / 10 * (level[(year - 2010) // 10 + 1] - level[(year - 2010) // 10])
+
+if __name__ == "__main__":
+    print(getLevel(2012))
